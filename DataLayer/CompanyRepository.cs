a7251d9cdf750477cd149a9c81971530db6ba36d
Igor Nigay <igor.nigay@gmail.com>
2017-03-21 01:16:44 -0700

    Adds DataLayer

---
+++ DataLayer/CompanyRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IMSDemo;
+using System.Data.Entity;
+
+namespace DataLayer
+{
+    public class CompanyRepository : ICompanyRepository, IDisposable
+    {
+        private IMSContext context;
+
+        public CompanyRepository(IMSContext context)
+        {
+            this.context = context;
+        }
+
+        public void DeleteCompany(int companyId)
+        {
+            Company company = context.Companies.Find(companyId);
+            context.Companies.Remove(company);
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if(!disposed)
+            {
+                if(disposing)
+                {
+                    context.Dispose();
+                }
+            }
+
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public List<Company> getCompanies()
+        {
+            return context.Companies.ToList();
+        }
+
+        public Company getCompany(int companyId)
+        {
+            return context.Companies.Find(companyId);
+        }
+
+        public void InsertCompany(Company company)
+        {
+            context.Companies.Add(company);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void UpdateCompany(Company company)
+        {
+            context.Entry(company).State = EntityState.Modified;
+        }
+    }
+}
---
+++ DataLayer/IMSContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using IMSDemo;
+
+namespace DataLayer
+{
+    public class IMSContext: DbContext
+    { 
+        public DbSet<Company> Companies { get; set; }
+
+        public DbSet<Inventory> Inventories { get; set; }
+
+        public DbSet<Location> Locations { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+    }
+}
